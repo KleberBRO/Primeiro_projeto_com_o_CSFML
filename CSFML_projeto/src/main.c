9c13b3e426be1b6d82ef18ccf3addadba568bebf
KleberBRO <kleber.fraga@ufrpe.br>
2023-11-26 13:21:37 -0300

    multiplos inimigos

--- CSFML_projeto/src/main.c
+++ CSFML_projeto/src/main.c
@@ -24,7 +24,8 @@ void velocidade(float *vx, float *vy) {
 int main() {
     srand(time(NULL));
     bool gameover = false;
-    int pontos = 0;
+    int pontos = 0, aux_pontos = 0;
+    int inimigos = 1;
 
     //gerar janela
     sfRenderWindow * janela;
@@ -63,11 +64,14 @@ int main() {
     sfSprite_setScale(sCirculo, (sfVector2f){0.1 ,0.1});
 
     //Sprite do circulo inimigo
-    sfSprite * sCirculo2;
-    sCirculo2 = sfSprite_create();
-    sfSprite_setTexture(sCirculo2, tCirculo, 0);
-    sfSprite_setScale(sCirculo2, (sfVector2f){0.1 ,0.1});
-    sfSprite_setColor(sCirculo2, sfColor_fromRGBA(255, 255, 0, 255));
+    sfSprite * sCirculo2[10];
+    for (int i = 0; i < 10; i++)
+    {
+        sCirculo2[i] = sfSprite_create();
+        sfSprite_setTexture(sCirculo2[i], tCirculo, 0);
+        sfSprite_setScale(sCirculo2[i], (sfVector2f){0.1 ,0.1});
+        sfSprite_setColor(sCirculo2[i], sfColor_fromRGBA(255, 255, 0, 255));
+    }
 
     //textura da moeda
     sfTexture * tMoeda;
@@ -82,12 +86,18 @@ int main() {
     //variáveis de posição e velocidade
     float x = 0.0, y = 0.0;
     float vx = 0.0, vy = 0.0;
-    float inimigo_X = 300, inimigo_y = 400;
+    float inimigo_X[10], inimigo_y[10];
+    inimigo_X[0] = 700;
+    inimigo_y[0] = 700;
     float moeda_X = 400, moeda_y = 400;
     
     //Hitbox do sprites
     sfFloatRect rect_Circulo = sfSprite_getLocalBounds(sCirculo);
-    sfFloatRect rect_Circulo2 = sfSprite_getLocalBounds(sCirculo2);
+    sfFloatRect rect_Circulo2[10];
+    for (int i = 0; i < 10; i++) {
+        rect_Circulo2[i] = sfSprite_getLocalBounds(sCirculo2[i]);
+    }
+    
     sfFloatRect rect_Moeda = sfSprite_getLocalBounds(sMoeda);
 
     // Define o tempo de atraso para 2 segundos
@@ -110,6 +120,7 @@ int main() {
             elapsedTime = sfClock_getElapsedTime(clock);
         }
 
+        //Mostra os pontos na tela
         char pontosStr[50];
         sprintf(pontosStr, "Pontos: %d", pontos);
         sfText_setString(textoPontos, pontosStr);
@@ -151,21 +162,47 @@ int main() {
             vx = - vx*0.1;
         }
 
-        // diferença entre as posições
-        float dx = x + (rect_Circulo.width*0.1/2) - (inimigo_X + (rect_Circulo2.width*0.1/2));
-        float dy = y + (rect_Circulo.height*0.1/2) - (inimigo_y + (rect_Circulo2.height*0.1/2));
+        float dx[10], dy[10], Distancia[10];
 
-        // Hipotenusa
-        float Distancia = sqrt(pow(dx, 2) + pow(dy, 2));
-        //caso eles se toquem
-        if (Distancia < (rect_Circulo.width*0.1/2) + (rect_Circulo2.width*0.1/2)) {
-            gameover = true;
+        for (int i = 0; i <= inimigos-1; i++)  {
+            dx[i] = x + (rect_Circulo.width*0.1/2) - (inimigo_X[i] + (rect_Circulo2[i].width*0.1/2));
+            dy[i] = y + (rect_Circulo.height*0.1/2) - (inimigo_y[i] + (rect_Circulo2[i].height*0.1/2));
+            Distancia[i] = sqrt(pow(dx[i], 2) + pow(dy[i], 2));
         }
 
-        sfVector2f direcao = {x - inimigo_X, y - inimigo_y};
+        for (int i = 0; i <= inimigos-1; i++)  {
+            if (Distancia[i] < (rect_Circulo.width*0.1/2) + (rect_Circulo2[i].width*0.1/2)) {
+                gameover = true;
+            }
+
+            sfVector2f direcao = {x - inimigo_X[i], y - inimigo_y[i]};
+
+            for (int j = 0; j <= inimigos-1; j++) {
+                if(i != j){
+                    // diferença entre as posições
+                    dx[i] = inimigo_X[i] + (rect_Circulo2[i].width*0.1/2) - (inimigo_X[j] + (rect_Circulo2[j].width*0.1/2));
+                    dy[i] = inimigo_y[i] + (rect_Circulo2[i].height*0.1/2) - (inimigo_y[j] + (rect_Circulo2[j].height*0.1/2));
+
+                    // Hipotenusa
+                    float distancia_inimigos = sqrt(pow(dx[i], 2) + pow(dy[i], 2));
+                    //caso eles se toquem
+                    if (distancia_inimigos < (rect_Circulo2[i].width*0.1/2) + (rect_Circulo2[j].width*0.1/2)) {
+                        if (Distancia[i]>Distancia[j])
+                        {
+                            direcao.x =  direcao.x*vx*0.1;
+                            direcao.y =  direcao.y*vy*0.1;
+                        }
+                        
+                    }
+                }
+            }
+
+            inimigo_X[i] += direcao.x*(100/Distancia[i]*0.01);
+            inimigo_y[i] += direcao.y*(100/Distancia[i]*0.01);
+            sfSprite_setPosition(sCirculo2[i], (sfVector2f){inimigo_X[i] ,inimigo_y[i]});
+        }
         
-        inimigo_X += direcao.x*(100/Distancia*0.02);
-        inimigo_y += direcao.y*(100/Distancia*0.02);
+
 
         // diferença entre as posições entre x e a moeda
         float dxM = x + (rect_Circulo.width*0.1/2) - (moeda_X + (rect_Moeda.width*0.3/2));
@@ -176,9 +213,26 @@ int main() {
         //caso eles x e moeda se toquem
         if (Distancia_moeda < (rect_Circulo.width*0.1/2) + (rect_Moeda.width*0.3/2)) {
             pontos++;
+            aux_pontos++;
             moeda_X = (rand() % 987);
             moeda_y = (rand() % 731);
         }
+
+        if (pontos == inimigos*3 && pontos == aux_pontos-inimigos+1)
+        {
+         inimigos++;
+         aux_pontos++;
+         float spawnX, spawnY;
+         do {
+            spawnX = (rand() % 1324 - 100);
+            spawnY = (rand() % 731 - 100);
+         } while (spawnX*spawnY >= 0 && spawnX*spawnY <= 1024*768);
+         
+
+         inimigo_X[inimigos-1] = spawnX;  
+         inimigo_y[inimigos-1] = spawnY;  
+        }
+        
         
 
 
@@ -186,13 +240,14 @@ int main() {
         //posiciona os sprites
         sfRenderWindow_clear(janela, fundo);
         sfSprite_setPosition(sCirculo, (sfVector2f){x ,y});
-        sfSprite_setPosition(sCirculo2, (sfVector2f){inimigo_X ,inimigo_y});
         sfSprite_setPosition(sMoeda, (sfVector2f){moeda_X ,moeda_y});
 
 
         //Renderiza a tela
         sfRenderWindow_drawSprite(janela, sCirculo, NULL);
-        sfRenderWindow_drawSprite(janela, sCirculo2, NULL);
+        for (int i = 1; i <= inimigos; i++)  {
+            sfRenderWindow_drawSprite(janela, sCirculo2[i-1], NULL);
+        }
         sfRenderWindow_drawSprite(janela, sMoeda, NULL);
         sfRenderWindow_drawSprite(janela, textoPontos, NULL);
         if (gameover) {
@@ -217,7 +272,10 @@ int main() {
     sfSprite_destroy(sMoeda);
     sfTexture_destroy(tCirculo);
     sfSprite_destroy(sCirculo);
-    sfSprite_destroy(sCirculo2);
+        for (int i = 0; i < 10; i++)
+    {
+        sfSprite_destroy(sCirculo2[i]);
+    }
     sfRenderWindow_destroy(janela);
     
 
